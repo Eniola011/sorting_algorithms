91cb283b06caa0d0e66018d552406f818260b151
Eniola011 <yettyagbalu@gmail.com>
2023-09-24 14:30:52 -0700

    quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,57 +4,63 @@
  * swap - swaps elements
  * @n: points to first element
  * @m: points to second element
+ * @array: an array
+ * @size: size of the array
  * Return: no value
  */
-void swap(int *n, int *m)
+void swap(int *array, size_t size, int *n, int *m)
 {
-	int tmp = *n;
-
-	*n = *m;
-	*m = tmp;
+	if (*n != *m)
+	{
+		*n = *n + *m;
+		*m = *n - *m;
+		*n = *n - *m;
+		print_array((const int *)array, size);
+	}
 }
 
 /**
  * lomuto_partition - finds the partition position
  * @array: points to array
+ * @size: size of array
  * @low: index of first element
  * @high: index of last element
  * Return: 1
  */
-int lomuto_partition(int *array, int low, int high)
+int lomuto_partition(int *array, size_t size, ssize_t low, ssize_t high)
 {
 	int point = array[high];
-	int a = (low - 1), b;
+	int a, b;
 
-	for (b = low; b < high; b++)
+	for (a = b = low; b < high; b++)
 	{
-		if (array[b] <= point)
+		if (array[b] < point)
 		{
-			a++;
-			swap(&array[a], &array[b]);
+			swap(array, size, &array[b], &array[a++]);
 		}
 	}
 
-	swap(&array[a + 1], &array[high]);
+	swap(array, size, &array[a], &array[high]);
 
-	return (a + 1);
+	return (a);
 }
 
 /**
  * QuickSort - sort function
  * @array: points to array
+ * @size: size of array
  * @low: index of first element
  * @high: index of last element
  * Return: no value
  */
-void QuickSort(int *array, int low, int high)
+void QuickSort(int *array, size_t size, ssize_t low, ssize_t high)
 {
 	if (low < high)
 	{
-		int point_idx = lomuto_partition(array, low, high);
+		size_t point_idx = lomuto_partition(array, size, low, high);
 
-		QuickSort(array, low, point_idx - 1);
-		QuickSort(array, point_idx + 1, high);
+		QuickSort(array, size, low, point_idx - 1);
+		QuickSort(array, size, point_idx + 1, high);
 	}
 }
 
@@ -67,10 +73,10 @@ void QuickSort(int *array, int low, int high)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
+	if (!array || !size)
 	{
 		return;
 	}
 
-	QuickSort(array, 0, size - 1);
+	QuickSort(array, size, 0, size - 1);
 }
